14e418809a8921a58d95a1cc687d20572bca21d4
c143mac <c143gmail.com>
2026-02-10 00:23:55 -0800

    fix bug

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-//        applicationId = "com.treeworld.gardener"
-        applicationId = "com.ycoft.c143"
+        applicationId = "com.treeworld.gardener"
+//        applicationId = "com.ycoft.c143"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
